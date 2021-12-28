c0ccf0ed4a7499cdf5bb2ad9f1f6928e1c84482d
Jacob Emerick <jemerick@shutterstock.com>
2021-12-27 17:01:02 -0700

    Day 16

---
+++ 16/1/index.php
@@ -0,0 +1,84 @@
+<?php
+
+$transmission = 'A0016C880162017C3686B18A3D4780';
+
+/**
+$handle = fopen('input.txt', 'r');
+while (($line = fgets($handle))) {
+  $transmission = trim($line);
+}
+ */
+
+// PHP trips over big numbers w/ base_convert, need to be careful
+$binary = '';
+for ($i = 0; $i < strlen($transmission); $i++) {
+  $binary .= str_pad(base_convert($transmission[$i], 16, 2), 4, '0', STR_PAD_LEFT);
+}
+
+function packetParser($string) {
+  $version = bindec(substr($string, 0, 3));
+  $type = bindec(substr($string, 3, 3));
+
+  if ($type === 4) {
+    $value = '';
+    $leftover = '';
+    $groups = str_split(substr($string, 6), 5);
+
+    foreach ($groups as $key => $group) {
+      $value .= substr($group, 1);
+      if ($group[0] === '0') {
+        $leftover = implode('', array_slice($groups, $key + 1));
+        break;
+      }
+    }
+
+    return [
+      'type' => 'literal',
+      'version' => $version,
+      'value' => bindec($value),
+      'leftover' => $leftover,
+    ];
+  }
+
+  $subpackets = [];
+  $subpacketString = '';
+
+  if (substr($string, 6, 1) === '0') {
+    $subpacketLength = bindec(substr($string, 7, 15));
+    $subpacketString = substr($string, 22, $subpacketLength);
+
+    while (preg_match('/[^0]/', $subpacketString)) {
+      $subpackets[] = packetParser($subpacketString);
+      $subpacketString = end($subpackets)['leftover'];
+    }
+
+    $subpacketString = substr($string, 22 + $subpacketLength);
+  } else {
+    $subpacketCount = bindec(substr($string, 7, 11));
+    $subpacketString = substr($string, 18);
+
+    while ($subpacketCount > count($subpackets)) {
+      $subpackets[] = packetParser($subpacketString);
+      $subpacketString = @end($subpackets)['leftover'];
+    }
+  }
+
+  return [
+    'type' => 'operator',
+    'version' => $version,
+    'contains' => $subpackets,
+    'leftover' => $subpacketString,
+  ];
+}
+
+$parsedPacket = [packetParser($binary)];
+
+function walkParsedPacket($totalVersion, $packet) {
+  $totalVersion += $packet['version'];
+  if ($packet['type'] === 'operator') {
+    $totalVersion += array_reduce($packet['contains'], 'walkParsedPacket', 0);
+  }
+  return $totalVersion;
+}
+
+echo array_reduce($parsedPacket, 'walkParsedPacket', 0);
